4e08351025e8887ecb33eee57ee3c8603df628d3
DESKTOP-7UI9CFC\User <davide>
2022-11-30 15:46:48 +0100

    mqtt

---
+++ iot-protocols/iot-protocols-mqtt/client/NetCoreClient/Program.cs
@@ -0,0 +1,26 @@
+﻿using NetCoreClient.Sensors;
+using NetCoreClient.Protocols;
+
+// define sensors
+List<ISensorInterface> sensors = new();
+sensors.Add(new VirtualSpeedSensor());
+
+// define protocol
+//IProtocolInterface protocol = new Http("http://localhost:8011/drones/123");
+IProtocolInterface protocol = new Mqtt("127.0.0.1");
+
+// send data to server
+while (true)
+{
+    foreach (ISensorInterface sensor in sensors)
+    {
+        var sensorValue = sensor.ToJson();
+
+        protocol.Send(sensorValue, sensor.GetSlug());
+
+        Console.WriteLine("Data sent: " + sensorValue);
+
+        Thread.Sleep(1000);
+    }
+
+}
