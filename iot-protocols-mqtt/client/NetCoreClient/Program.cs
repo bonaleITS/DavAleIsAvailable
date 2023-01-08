d540b8986aa8aaa9422dc3fece1a06644fd7b0c6
Piccihud <105939627+PicciHud@users.noreply.github.com>
2023-01-08 16:37:08 +0100

    iot-protocols-mqtt
    Signed-off-by: Piccihud <105939627+PicciHud@users.noreply.github.com>

---
+++ iot-protocols-mqtt/client/NetCoreClient/Program.cs
@@ -0,0 +1,28 @@
+﻿using NetCoreClient.Sensors;
+using NetCoreClient.Protocols;
+
+// define sensors
+List<ISensorInterface> sensors = new();
+sensors.Add(new VirtualSpeedSensor());
+sensors.Add(new VirtualPositionSensor());
+sensors.Add(new VirtualAltitudeSensor());
+sensors.Add(new VirtualChargeSensor());
+
+// define protocol
+IProtocolInterface protocol = new Mqtt("127.0.0.1");
+
+// send data to server
+while (true)
+{
+    foreach (ISensorInterface sensor in sensors)
+    {
+        var sensorValue = sensor.ToJson();
+
+        protocol.Send(sensorValue, sensor.GetSlug());
+
+        Console.WriteLine("Data sent: " + sensorValue + sensor.GetSlug());
+
+        Thread.Sleep(1000);
+    }
+        
+}
---
+++ iot-protocols-mqtt/client/NetCoreClient/ValueObjects/Charge.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetCoreClient.ValueObjects
+{
+    internal class Charge
+    {
+        public string Name { get; private set; }
+        public int Value { get; private set; }
+        public DateTime Date { get; private set; }
+
+        public Charge(int value)
+        {
+            this.Name = "Charge";
+            this.Value = value;
+            this.Date = DateTime.Now;
+        }
+    }
+}
